336c0dd73a0bc28dd2d321a19189fffa84c33967
Olateju Victor Daniel <christianovik009@gmail.com>
2022-02-16 19:41:45 +0100

    start file

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+// app.use(parser.urlencoded({ extended: true }));
 // app.use('*', cors())
 
 let mysqlConnection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: '',
-	database: 'capstone',
-	multipleStatements: true
+	database: 'tqbk',
 })
 
 mysqlConnection.connect((err=>{
@@ -66,11 +68,69 @@ const productArr = [
    {id:3, product_name:'Channel Bag', price: 1000},
 ]
 
+const cartItems = [];
 
 app.get("/product", function(req, res, next){
-     res.json({message:'Products returned successfully', products: 
+     res.render('home', {message:'Products returned successfully', products: 
+productArr, cart: cartItems})
+})
+
+app.post('/add', function(req, 	res){
+	const id = Number(req.body.id);
+	//find the product form productArr via id
+	const findProduct = productArr.find(productItem => productItem.id === id)
+	//next
+	//add the found product to the cartItems arr
+
+
+	//Check if item exiosts in cart already
+	const checkCart = cartItems.find(item=>item.id === id)
+	
+  if (checkCart) {
+    (checkCart.quantity += 1);
+  } else {
+     cartItems.push({ ...findProduct, quantity: 1 });
+  }
+	
+	// res.json({message:'Item Added to cart successfully'});
+	// console.log("Product id from the frontend ",id)
+	res.redirect('/product')
+})
+
+
+app.get("/about", function(req, res, next){
+     res.render('index', {message:'Products returned successfully', products: 
 productArr})
 })
+
+
+
+
+
+
+
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
